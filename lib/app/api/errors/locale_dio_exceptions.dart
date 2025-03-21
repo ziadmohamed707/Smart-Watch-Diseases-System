0a95f227e81ac608fc523ff7198f17d17f00c98a
Ziad Mohamed <ziadm707.zm@gmail.com>
2025-03-21 21:28:02 +0200

    Last Version Update

--- lib/app/api/errors/locale_dio_exceptions.dart
+++ lib/app/api/errors/locale_dio_exceptions.dart
@@ -1,39 +1,39 @@
-import 'package:iot_monitoring_chronic_diseases_system/utils/locale/app_localization_keys.dart';
-
-
-class LocaleDioExceptions {
-  static String getLocaleMessage(int code) {
-    switch (code) {
-      case 1001:
-        return LocalizationKeys.requestToApiServerWasCancelled;
-      case 1002:
-        return LocalizationKeys.connectionTimeoutWithApiServer;
-      case 1003:
-        return LocalizationKeys.receiveTimeoutInConnectionWithApiServer;
-      case 1004:
-        return LocalizationKeys.badRequest;
-      case 1005:
-        return LocalizationKeys.unauthorized;
-      case 1006:
-        return LocalizationKeys.forbidden;
-      case 1008:
-        return LocalizationKeys.internalServerError;
-      case 1009:
-        return LocalizationKeys.badGateway;
-      case 1010:
-        return LocalizationKeys.oopsSomethingWentWrong;
-      case 1011:
-        return LocalizationKeys.sendTimeoutInConnectionWithApiServer;
-      case 1012:
-        return LocalizationKeys.noInternet;
-      case 1013:
-        return LocalizationKeys.unexpectedErrorOccurred;
-      case 1014:
-        return LocalizationKeys.somethingWentWrong;
-      case 1015:
-        return LocalizationKeys.unknownError;
-      default:
-        return LocalizationKeys.unknownError;
-    }
-  }
-}
+// import 'package:iot_monitoring_chronic_diseases_system/utils/locale/app_localization_keys.dart';
+//
+//
+// class LocaleDioExceptions {
+//   static String getLocaleMessage(int code) {
+//     switch (code) {
+//       case 1001:
+//         return LocalizationKeys.requestToApiServerWasCancelled;
+//       case 1002:
+//         return LocalizationKeys.connectionTimeoutWithApiServer;
+//       case 1003:
+//         return LocalizationKeys.receiveTimeoutInConnectionWithApiServer;
+//       case 1004:
+//         return LocalizationKeys.badRequest;
+//       case 1005:
+//         return LocalizationKeys.unauthorized;
+//       case 1006:
+//         return LocalizationKeys.forbidden;
+//       case 1008:
+//         return LocalizationKeys.internalServerError;
+//       case 1009:
+//         return LocalizationKeys.badGateway;
+//       case 1010:
+//         return LocalizationKeys.oopsSomethingWentWrong;
+//       case 1011:
+//         return LocalizationKeys.sendTimeoutInConnectionWithApiServer;
+//       case 1012:
+//         return LocalizationKeys.noInternet;
+//       case 1013:
+//         return LocalizationKeys.unexpectedErrorOccurred;
+//       case 1014:
+//         return LocalizationKeys.somethingWentWrong;
+//       case 1015:
+//         return LocalizationKeys.unknownError;
+//       default:
+//         return LocalizationKeys.unknownError;
+//     }
+//   }
+// }
--- lib/app/api/repositories/auth_repository.dart
+++ lib/app/api/repositories/auth_repository.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:iot_monitoring_chronic_diseases_system/app/token/token_storage.dart';
 
